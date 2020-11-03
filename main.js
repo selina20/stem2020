15db0001bbcd254a10e4a083c2dba07230563624
selina20 <yuminselinalee@gmail.com>
2020-11-03 18:25:44 +0900

    third

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
 var bar = new ProgressBar.Circle(container, {
-  color: '#aaa',
+  color: '#ff6347',
   // This has to be the same size as the maximum width to
   // prevent clipping
   strokeWidth: 4,
@@ -12,8 +12,8 @@ var bar = new ProgressBar.Circle(container, {
   text: {
     autoStyleContainer: false
   },
-  from: { color: '#aaa', width: 1 },
-  to: { color: '#333', width: 4 },
+  from: { color: '#fffff0', width: 1 },
+  to: { color: ' #ff6347', width: 4 },
   // Set default step function for all animate calls
   step: function(state, circle) {
     circle.path.setAttribute('stroke', state.color);
@@ -28,6 +28,7 @@ var bar = new ProgressBar.Circle(container, {
 
   }
 });
+
 bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
 bar.text.style.fontSize = '2rem';
 
